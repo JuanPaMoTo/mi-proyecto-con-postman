dc34a3dc76d4fd962d0221ca4bb7f6b84af6ed7b
JuanPaMoTo <fmsuesca@gmail.com>
2026-09-18 14:50:22 -0300

    Fix: servir inde.html desde express

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const path = require('path');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-// Middleware para parsear JSON en las peticiones
 app.use(express.json());
 
-// Montaje de rutas
+// Servir archivos estáticos (index.html, main.js, etc.)
+app.use(express.static(path.join(__dirname, '../')));
+
+// Rutas de la API
 app.use('/api', userRoutes);
+
+// Ruta de respaldo para entregar index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
 
 module.exports = app;
