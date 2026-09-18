18f3a40d2157ea7db3686dfea3acc45e35b71844
JuanPaMoTo <fmsuesca@gmail.com>
2026-09-18 14:53:52 -0300

    Fix: configuración limpia de Vercer rewrites

--- src/app.js
+++ src/app.js
@@ -1,20 +1,11 @@
 const express = require('express');
-const path = require('path');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(express.json());
 
-// Servir archivos estáticos (index.html, main.js, etc.)
-app.use(express.static(path.join(__dirname, '../')));
-
-// Rutas de la API
+// Montaje de rutas
 app.use('/api', userRoutes);
-
-// Ruta de respaldo para entregar index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
-});
 
 module.exports = app;
